1f650171c6194523c2303fefb09c36d4b439090e
Khaled Gasmi <khaledgs1mi@gmail.com>
2022-07-13 15:08:39 +0100

    changes to models file

--- authentication/models.py
+++ authentication/models.py
@@ -60,4 +60,58 @@ class Courses(models.Model):
     class Meta:
         ordering = ['c_name']
 
+
+#creating the payment pethod table
+class P_method(models.Model):
+    P_id = models.UUIDField()
+    p_name = models.CharField(max_length=50)
+
+#creating the transactions table 
+class Transactions(models.Model):
+    tu_id = models.ForeignKey(Teachers, on_delete=models.CASCADE, null=True, blank=True)
+    su_id = models.ForeignKey(Students, on_delete=models.CASCADE, null=True, blank=True)
+    c_id = models.ForeignKey(Courses, on_delete=models.CASCADE, null=True, blank=True)
+    p_method = models.ForeignKey(P_method, on_delete=models.CASCADE, null=True, blank=True)
+    transaction_time = models.DateTimeField()
+    price = models.FloatField()
+    f_price = models.FloatField()
+    
+
+    def __str__(self):
+        return self.su_id, self.c_id, self.transaction_time 
+
+    class Meta:
+        ordering = ['transaction_time']
         
+#creating the courses comments table
+class C_comment(models.Model):
+    tu_id = models.ForeignKey(Teachers, on_delete=models.CASCADE, null=True, blank=True)
+    su_id = models.ForeignKey(Students, on_delete=models.CASCADE, null=True, blank=True)
+    c_id = models.ForeignKey(Courses, on_delete=models.CASCADE, null=True, blank=True)
+    comment = models.BinaryField()
+    comment_time = models.DateTimeField()
+
+#creating the wallet table
+class C_comment(models.Model):
+    tu_id = models.ForeignKey(Teachers, on_delete=models.CASCADE, null=True, blank=True)
+    su_id = models.ForeignKey(Students, on_delete=models.CASCADE, null=True, blank=True)
+    amo_bfor = models.FloatField()
+    amo_after = models.FloatField()
+    change_or_buy = models.IntegerField()
+    time = models.DateTimeField()
+    p_method = models.ForeignKey(P_method, on_delete=models.CASCADE, null=True, blank=True)
+
+#creating the post table
+class Post(models.Model):
+    p_id = models.UUIDField()
+    tu_id = models.ForeignKey(Teachers, on_delete=models.CASCADE, null=True, blank=True)
+    su_id = models.ForeignKey(Students, on_delete=models.CASCADE, null=True, blank=True)
+    post = models.BinaryField()
+
+#creating the replay post
+class Replay(models.Model):
+    p_id = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, blank=True)
+    tu_id = models.ForeignKey(Teachers, on_delete=models.CASCADE, null=True, blank=True)
+    su_id = models.ForeignKey(Students, on_delete=models.CASCADE, null=True, blank=True)
+    replay = models.BinaryField()
+
